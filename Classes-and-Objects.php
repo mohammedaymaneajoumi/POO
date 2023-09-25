8472ce23ee31e0284fbb81767a83758363881a95
mohammedaymaneajoumi <aymanelajoumi@gmail.com>
2023-09-25 12:34:51 +0100

    update

--- Classes-and-Objects.php
+++ Classes-and-Objects.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <div class="container">
+        <?php
+        class Fruit
+        {
+            // Properties
+            public $name;
+            public $color;
+
+            // Methods
+            function set_name($name)
+            {
+                $this->name = $name;
+            }
+            function get_name()
+            {
+                return $this->name;
+            }
+        }
+
+        $apple = new Fruit();
+        $banana = new Fruit();
+        $apple->set_name('Apple');
+        $banana->set_name('Banana');
+
+        echo $apple->get_name();
+        echo "<br>";
+        echo $banana->get_name();
+        ?>
+        
+    </div>
 </body>
+
 </html>
